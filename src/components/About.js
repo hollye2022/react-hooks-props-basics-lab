00d7d2605aa5cf7a7c767b63aaf069c98b097416
chengying <hollye2558@gmail.com>
2022-12-20 11:27:52 -0500

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,22 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+
+  
+
+  let bio;
+  if (props.bio!=="" && props.bio!==undefined) {
+    bio = <p>{props.bio}</p>
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
       {/* add your <Links /> component here */}
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function Home(props) {
+ const divStyle = {
+  color: props.color
+ }
+  
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
+      <h1 style={divStyle}>
         {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
   );
+  
 }
 
 export default Home;
